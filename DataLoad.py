2ba5696a3072d80912f22458e5c405f49dd0704a
Boolton <67080752+Boolton@users.noreply.github.com>
2020-06-18 02:08:58 +0200

    Add files via upload

---
+++ DataLoad.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 01:48:10 2020
+
+@author: Peter
+"""
+import numpy as np
+import pandas as pd
+
+def dataLoad(filename):
+    df = pd.read_csv('test.txt', delimiter = ' ')
+#Adding headers
+    df.columns=['Temperature', 'Growth rate', 'Bacteria species']
+
+#Converting frame to array
+    dataI = df.iloc[:,0:3].to_numpy()
+#Creating bool from
+    boolEval = np.ones((np.size(dataI, axis = 0), np.size(dataI,axis = 1)), dtype=bool)
+    
+# going through each row of the data matrix
+    for y in range(np.size(dataI, 0)):
+    
+    # validation of the temperatures
+        if ((dataI[y,0] < 10) or (dataI[y,0] > 60)):
+            boolEval[y,0] = False
+
+    # validation of the growth rates
+        if (dataI[y,1] < 0):
+            boolEval[y,1] = False
+                 
+    # validation of the bacteria species
+        if ((dataI[y,2] < 1) or (dataI[y,2] > 4)):
+            boolEval[y,2] = False
+        
+        y = y + 1
+
+    for x in range(np.size(dataI,0)):
+    
+    #All data
+        if (boolEval[x,0] == False) and (boolEval[x,1] == False) and (boolEval[x,2] == False):
+            print("Erroneous data found. All attributes out of bounds. Line {:.0f} deleted!".format(x))
+    #Temp and growth
+        elif (boolEval[x,0] == False) and (boolEval[x,1] == False):
+            print("Erroneous data found. Temperature and growth rate out of bounds. Line {:.0f} deleted!".format(x))
+    #Temp and bacteria
+        elif (boolEval[x,0] == False) and (boolEval[x,2] == False):
+            print("Erroneous data found. Temperature and bacteria species out of bounds. Line {:.0f} deleted!".format(x))
+    #Growth and bacteria    
+        elif (boolEval[x,1] == False) and (boolEval[x,2] == False):
+            print("Erroneous data found. Growth rate and bacteria species out of bounds. Line {:.0f} deleted!".format(x))
+    #Temp    
+        elif (boolEval[x,0] == False):
+            print("Erroneous data found. Temperature out of bounds. Line {:.0f} deleted!".format(x))
+    #Growth    
+        elif (boolEval[x,1] == False):
+            print("Erroneous data found. Growth rate out of bounds. Line {:.0f} deleted!".format(x))
+    #Bacteria    
+        elif (boolEval[x,2] == False):
+            print("Erroneous data found. Bacteria species out of bounds. {:.0f} Line deleted!".format(x))
+            
+
+# Indexing temperature and filtering erroneous data
+    indexTemp = df[(df['Temperature'] > 60 ) | (df['Temperature'] < 10)].index
+    data = df.drop(indexTemp)
+# Indexing growth rate and filtering erroneous data    
+    indexGrowth = data[(data['Growth rate'] < 0 )].index
+    data = data.drop(indexGrowth)
+# Indexing bacteria species and filtering erroneous data
+    indexBact = data[(data['Bacteria species'] > 4 ) | (data['Bacteria species'] < 1)].index
+    data = data.drop(indexBact)
+
+
+#Write new txt file
+    df.to_csv('data.txt', index=False, sep=' ')
+    
+    return data
+
---
+++ DataPlot.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 18:53:57 2020
+
+@author: Peter
+"""
+from DataLoad import dataLoad
+filename = "test.txt"
+
+def dataPlot(data):
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+#Counting up the bacteria species by type
+    bacNum, bacCount =  np.unique(data.iloc[:,2], return_counts=True)
+    
+#Setting bacteria labels
+    bacName = ['Salmonella enterica', 'Bacillus cereus', 'Listeria', 'Brochothrix thermosphacta']
+
+#Plotting bar graph
+    fig = plt.gcf()
+    fig.set_size_inches(10, 6)
+    plt.bar(bacNum,bacCount, color=['#0b162a','#c83803','#97233f','#008e97'])
+    plt.xticks(bacNum,(bacName),fontsize=8)
+    plt.yticks()
+    plt.title("Bacteria quantities",fontsize=18)
+    plt.xlabel("Species of bacteria",fontsize=12)
+    plt.ylabel("Number of measurements",fontsize=12)
+    for a,b in zip(bacNum, bacCount):
+        plt.text(a, b, str(b))
+    plt.savefig('BacteriaQuantities')
+    plt.show()
+
+#Split original dataframe into four, according to bacteria species
+    data1 = data[data['Bacteria species'] == 1].sort_values(['Temperature'])
+    data2 = data[data['Bacteria species'] == 2].sort_values(['Temperature'])
+    data3 = data[data['Bacteria species'] == 3].sort_values(['Temperature'])
+    data4 = data[data['Bacteria species'] == 4].sort_values(['Temperature'])
+
+#Plotting growth rate over temperature
+    fig = plt.gcf()
+    fig.set_size_inches(10, 6)
+    plt.plot('Temperature','Growth rate',data=data1, color='#0b162a')
+    plt.plot('Temperature','Growth rate',data=data2, color='#c83803')
+    plt.plot('Temperature','Growth rate',data=data3, color='#97233f')
+    plt.plot('Temperature','Growth rate',data=data4, color='#008e97')
+    plt.title("Growth rate as a function of temperature",fontsize=18)
+    plt.xlabel("Temperature",fontsize=12)
+    plt.ylabel("Growth rate",fontsize=12)
+    plt.legend(['Salmonella enterica', 'Bacillus cereus', 'Listeria', 'Brochothrix thermosphacta'],bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.savefig('GrowthTemp')
+    plt.show()
